f0dba05b9e0e8ff7ea021a662e751ca5106162a5
Joe Nixon <joe.nixon@motivedrilling.com>
2018-07-24 16:23:04 -0500

    New git dirty check

--- pre-push.sh
+++ pre-push.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 echo "Running pre-push script..."
 ./sync.sh
-if git diff-index --quiet HEAD -- || false
+if [[ -z $(git status -s) ]]
 then
+    echo "Pushing..."
+else
     echo "Committing last changes..."
     git add .
     git commit -m "File Sync"
-    git push origin master
 fi
