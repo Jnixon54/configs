52786efa458e0d521d74173d15cb5b2c59a47cc7
Joe Nixon <joe.nixon@motivedrilling.com>
2018-07-24 16:34:37 -0500

    Brewfile

--- sync.sh
+++ sync.sh
@@ -6,6 +6,8 @@
 
 echo "Syncing Config Files. "
 
+rm -rf Brewfile
+brew bundle dump
 rsync -a ${HOME}/dotfiles ${HOME}/configs/
 rsync -a ${HOME}/bin ${HOME}/configs/
 rsync -a ${HOME}/Brewfile ${HOME}/configs/Brewfile
