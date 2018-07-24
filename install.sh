42982402474effe34bff64f4f323baffab46314e
Joe Nixon <joe.nixon@motivedrilling.com>
2018-07-24 15:53:42 -0500

    testing hooks

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 chmod +x pre-commit.sh
 chmod +x pre-push.sh
 chmod +x sync.sh
-ln -s ./pre-commit.sh .git/hooks/pre-commit.sh
-ln -s ./pre-push.sh .git/hooks/pre-push.sh
+ln -s ./pre-commit.sh .git/hooks/pre-commit
+ln -s ./pre-push.sh .git/hooks/pre-push
